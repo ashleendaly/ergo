fe300d256aa7149090914a28c2163e88bdf3b9d8
Connor <2470724c@student.gla.ac.uk>
2024-10-12 22:59:53 +0100

    fixed blue on cards

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -12,15 +12,15 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, address, shortAddress, longitude, latitude, status }) => {
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg dark:border-gray-700">
         <img src={blackDroneIcon} />
       <a href="#">
-        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-black">{title}</h5>
       </a>
-      <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{shortAddress}</p>
-      <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{longitude}</p>
-      <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{latitude}</p>
-      <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{status}</p>
+      <p className="mb-3 font-normal text-gray-500 dark:text-black">{shortAddress}</p>
+      <p className="mb-3 font-normal text-gray-500 dark:text-black">{longitude}</p>
+      <p className="mb-3 font-normal text-gray-500 dark:text-black">{latitude}</p>
+      <p className="mb-3 font-normal text-gray-500 dark:text-black">{status}</p>
       <a href={`https://sepolia.etherscan.io/address/${address}`} className="inline-flex font-medium items-center text-blue-600 hover:underline">
          See contract
         <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
