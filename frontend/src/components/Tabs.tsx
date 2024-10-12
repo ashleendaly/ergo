c891541286a3a356554e3caaf9cc77635c7809a0
2776897J <2776897J@student.gla.ac.uk>
2024-10-12 17:39:28 +0100

    remove comments

--- frontend/src/components/Tabs.tsx
+++ frontend/src/components/Tabs.tsx
@@ -14,8 +14,8 @@ const Tabs: React.FC<TabsProps> = ({ activeTab, onTabClick }) => {
           onClick={() => onTabClick(0)}
           className={`inline-block px-4 py-3 rounded-lg ${
             activeTab === 0 
-              ? 'text-[#171717] bg-[#f4f4f4]'  // Active tab color
-              : 'text-white bg-[#171717] hover:bg-gray-300'  // Inactive tab color
+              ? 'text-[#171717] bg-[#f4f4f4]'
+              : 'text-white bg-[#171717] hover:bg-gray-300'
           }`}
           aria-current={activeTab === 0 ? 'page' : undefined}
         >
@@ -28,8 +28,8 @@ const Tabs: React.FC<TabsProps> = ({ activeTab, onTabClick }) => {
           onClick={() => onTabClick(1)}
           className={`inline-block px-4 py-3 rounded-lg ${
             activeTab === 1 
-              ? 'text-[#171717] bg-[#fafafa]'  // Active tab color
-              : 'text-white bg-[#171717] hover:bg-gray-300'  // Inactive tab color
+              ? 'text-[#171717] bg-[#fafafa]'
+              : 'text-white bg-[#171717] hover:bg-gray-300'
           }`}
           aria-current={activeTab === 1 ? 'page' : undefined}
         >
