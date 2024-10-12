1e5d142dff9b230500033f686b51680ffcce218c
cammyearly <2762365e@student.gla.ac.uk>
2024-10-12 17:18:11 +0100

    allowing CORS

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 from Drone import Drone
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     message: str
