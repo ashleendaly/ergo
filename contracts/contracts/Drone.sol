754dcaaed219c6bebfcc2d603130f93b60893697
Ash <ashddev@proton.me>
2024-10-12 16:00:28 +0100

    working drone deployed

---
+++ contracts/contracts/Drone.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Drone {
+    int256 public latitude;
+    int256 public longitude;
+
+    constructor(int256 _latitude, int256 _longitude) {
+        latitude = _latitude;
+        longitude = _longitude;
+    }
+
+    function sendDrone(int256 _latitude, int256 _longitude) public {
+        int256 incrementAmountLatitude = _latitude/10;
+        int256 incrementAmountLongitude = _longitude/10;
+        updateLocation(incrementAmountLatitude, incrementAmountLongitude);
+    }
+
+    function updateLocation(int256 _latitude, int256 _longitude) public {
+        latitude = _latitude;
+        longitude = _longitude;
+    }
+
+    function getLocation() public view returns (int256, int256) {
+        return (latitude, longitude);
+    }
+}
