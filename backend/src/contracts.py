24b22938e822b700934a2635989a01401098ff98
cammyearly <2762365e@student.gla.ac.uk>
2024-10-13 03:02:24 +0100

    added code for making bids on a package

--- backend/src/contracts.py
+++ backend/src/contracts.py
@@ -16,6 +16,12 @@ def getLocation(address):
     location = contract_instance.functions.getLocation().call()
     return (location[0] / 10000, location[1] / 10000)
 
+def make_bid(address, pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude):
+    checksum_address = w3.to_checksum_address(address)
+    contract_instance = w3.eth.contract(address=checksum_address, abi=contract_abi)
+    bid = contract_instance.functions.makeBid(pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude).call()
+    return bid
+
 def send_transaction(address, lat, long):
     checksum_address = w3.to_checksum_address(address)
     contract_instance = w3.eth.contract(address=checksum_address, abi=contract_abi)
@@ -28,8 +34,8 @@ def send_transaction(address, lat, long):
     
     nonce = w3.eth.get_transaction_count(account)  # Get nonce for the transaction
     transaction = contract_instance.functions.updateLocation(
-        int(lat),  
-        int(long)
+        int(location[0] - lat_increment),  # Increment location coordinates
+        int(location[1] - long_increment)
     ).build_transaction({
         'from': account,
         'nonce': nonce,
