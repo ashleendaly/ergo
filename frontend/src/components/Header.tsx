8009511a8a9f43f90dbaafd2f9bb13aa2c1996dc
Ash <ashddev@proton.me>
2024-10-21 14:04:50 +0100

    remove mw

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,6 @@ const Header: React.FC = () => {
     <header className="header">
         <div className='flex-container'>
         <div className="logos">
-            <img src="/images/mw-logo.png" alt="MW Logo" className="logo-image" />
             <img src="/images/ergo-logo.png" alt="Ergo Logo" className="logo-image" />
         </div>
       </div>    
